9f08b8fb08b116c49ca0d6e95f63f751405b27f0
Isaac Kinley <isaac.kinley@gmail.com>
2023-08-03 17:11:12 -0400

    LOTR and margins

--- lotr.js
+++ lotr.js
@@ -1,11 +1,10 @@
 
-var pi_99 = (
+var lotr = (
 	function() {
 		var likert_labels = [
 			"Strongly disagree",
 			"Disagree",
-			"Slightly disagree",
-			"Slightly agree",
+			"Neutral",
 			"Agree",
 			"Strongly agree"
 		];
@@ -129,32 +128,44 @@ var pi_99 = (
 				{name: "e2",	labels: likert_labels, prompt: "No matter where we are, incredible beauty is always around us."}
 			])
 		}
-		var n_pages = 3;
-		var n_per_page = 34;
-		var timeline = [];
-		var i, k, trial;
-		for (i = 0; i < n_pages; i++) {
-			trial = {
-				type: jsPsychSurveyLikert,
-				preamble: survey_info.instr['page_' + (i + 1)],
-				questions: [],
-				scale_width: 500,
-				on_start: function() {
-					var el = jsPsych.getDisplayElement();
-					el.style.marginTop = '100px';
-					el.style.marginBottom = '100px';
-				},
-				on_finish: function() {
-					var el = jsPsych.getDisplayElement();
-					el.style.marginTop = '';
-					el.style.marginBottom = '';
-				}
+		var trial = {
+			type: jsPsychSurveyLikert,
+			preamble: 'Please answer the following questions about yourself by indicating the extent of your agreement. Be as honest as you can throughout, and try not to let your responses to one question influence your responses to other questions. There are no right or wrong answers.',
+			questions: [
+				{labels: likert_labels, prompt: "In uncertain times, I usually expect the best."},
+				{labels: likert_labels, prompt: "It's easy for me to relax."},
+				{labels: likert_labels, prompt: "If something can go wrong for me, it will."},
+				{labels: likert_labels, prompt: "I'm always optimistic about my future."},
+				{labels: likert_labels, prompt: "I enjoy my friends a lot."},
+				{labels: likert_labels, prompt: "It's important for me to keep busy."},
+				{labels: likert_labels, prompt: "I hardly ever expect things to go my way."},
+				{labels: likert_labels, prompt: "I don't get upset too easily."},
+				{labels: likert_labels, prompt: "I rarely count on good things happening to me."},
+				{labels: likert_labels, prompt: "Overall, I expect more good things to happen to me than bad."}
+			],
+			scale_width: 500,
+			on_start: function() {
+				var el = jsPsych.getDisplayElement();
+				el.style.marginTop = '100px';
+				el.style.marginBottom = '100px';
+			},
+			on_finish: function() {
+				var el = jsPsych.getDisplayElement();
+				el.style.marginTop = '';
+				el.style.marginBottom = '';
 			}
-			for (k = 0; k < n_per_page; k++) {
-				trial.questions.push(survey_info.questions.pop());
-			}
-			timeline.push(trial);
 		}
-		return({timeline: timeline});
+		return(trial);
 	}
 )();
+
+
+
+
+
+
+
+
+
+
+
